3143df1f20a15529eba7acfa8c7f5f7b62ebd40a
Sam Skinner <sam.sjskinner@gmail.com>
2021-05-03 20:20:49 +1000

    Location now accepts x and y coords and perfors Equality off both

--- GoL/Location.cs
+++ GoL/Location.cs
@@ -1,12 +1,34 @@
+using System;
+
 namespace GoL
 {
     public class Location
     {
         private readonly int _xPosition;
+        private readonly int _yPosition;
 
-        public Location(int x)
+        public Location(int x, int y)
         {
             _xPosition = x;
+            _yPosition = y;
+        }
+
+        protected bool Equals(Location other)
+        {
+            return _xPosition == other._xPosition && _yPosition == other._yPosition;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Location) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_xPosition, _yPosition);
         }
     }
 }
--- GoLTests/LocationTests.cs
+++ GoLTests/LocationTests.cs
@@ -5,10 +5,11 @@ namespace GoLTests
 {
     public class LocationTests
     {
+        [Fact]
         public void TwoLocationsWithSameCoordinates_ShouldEqual()
         {
-            Location location1 = new Location(1);
-            Location location2 = new Location(1);
+            Location location1 = new Location(1, 2);
+            Location location2 = new Location(1, 2);
             
             Assert.Equal(location1, location2);
         }
